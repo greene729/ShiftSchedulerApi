f378bec22c460c0163b13e4c04df2d5c013eb4e6
Alex Greene <alex@roofsnap.com>
2019-08-13 09:52:46 -0400

    initial commit

---
+++ Data/ShiftSchedulerDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShiftSchedulerApi.Models;
+
+namespace ShiftSchedulerApi.Data
+{
+    public class ShiftSchedulerDbContext : DbContext
+    {
+        public ShiftSchedulerDbContext(DbContextOptions<ShiftSchedulerDbContext> options) : base(options)
+        {
+
+        }
+
+            public DbSet<Shift> Shifts { get; set; }
+            public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/Shift.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShiftSchedulerApi.Models
+{
+    public class Shift
+    {
+        public long Id { get; set; }
+        public string CreatedByUserId { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Description { get; set; }
+        public ICollection<User> Attendees { get; set; }
+        
+    }
+}
